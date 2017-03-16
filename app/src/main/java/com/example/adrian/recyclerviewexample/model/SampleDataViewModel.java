c1a53145eeddbe2c9d5eac1c07bf45483f745b12
Adrian Czigany <adrian_czigany@epam.com>
2017-03-16 10:20:16 +0100

    mod

--- app/src/main/java/com/example/adrian/recyclerviewexample/model/SampleDataViewModel.java
+++ app/src/main/java/com/example/adrian/recyclerviewexample/model/SampleDataViewModel.java
@@ -18,30 +18,6 @@ public class SampleDataViewModel {
         this.text2 = text2;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getText1() {
-        return text1;
-    }
-
-    public void setText1(String text1) {
-        this.text1 = text1;
-    }
-
-//    public String getText2() {
-//        return text2;
-//    }
-//
-//    public void setText2(String text2) {
-//        this.text2 = text2;
-//    }
-
     @Override
     public String toString() {
         return "SampleDataViewModel{" +
